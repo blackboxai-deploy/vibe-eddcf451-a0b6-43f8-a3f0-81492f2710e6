13fdfabfa23e292259b360b115db0ba3a64521df
blackboxai-deploy <deploy@blackboxai.dev>
2025-08-14 02:08:25 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/***********************
+ * Next.js configuration
+ **********************/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+
+module.exports = nextConfig;
